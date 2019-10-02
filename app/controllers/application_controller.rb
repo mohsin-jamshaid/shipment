fee985faf8cd82027421bd5b4ad19468624a5f38
mohsin-jamshaid <mohsin.jamshaid007@gmail.com>
2019-10-02 21:14:32 +0500

    Make email permittable as it is not an authenticated key anymore

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[markup_key contact_number company_name city username])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[markup_key contact_number company_name city username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email contact_number city username])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[email contact_number city username])
   end
 end
